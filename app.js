d65b08d97c92b1e0d2ea9fb2c2ffc4e002599126
Júlia Mendonça Silva <juliamendoncasilva@gmail.com>
2025-01-21 10:59:17 -0300

    Adicionando os amigos na lista e exibindo a lista pelo HTML

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+
+function adicionarAmigo(){
+    let amigo = document.querySelector('input').value;
+    if (amigo == ""){
+        alert("Por favor, insira um nome");
+    }else{
+        listaDeAmigos.push(amigo);  
+        exibeAmigos();
+    }
+    limparCampo();
+}
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function exibeAmigos(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    for (i = 0; i < (listaDeAmigos.length); i++){
+        let listaLi = document.createElement('li'); // cria um elemento tipo 'li'
+        listaLi.textContent = listaDeAmigos[i]; // define o texto do li como o nome naquela posição
+        lista.appendChild(listaLi); // adiciona <li> ao <ul> 
+    }
+    
+
+}
