50077aa3143f73ca9fbfa08081d046b279bd6504
Júlia Mendonça Silva <juliamendoncasilva@gmail.com>
2025-01-21 11:33:45 -0300

    Adição de verificações

--- app.js
+++ app.js
@@ -1,37 +1,58 @@
 let listaDeAmigos = [];
+let listaDeSorteados = [];
 
-function adicionarAmigo(){
+function adicionarAmigo() {
     let amigo = document.querySelector('input').value;
-    if (amigo == ""){
+    if (amigo == "") {
         alert("Por favor, insira um nome");
-    }else{
-        listaDeAmigos.push(amigo);  
+    } else {
+        listaDeAmigos.push(amigo);
         exibeAmigos();
     }
     limparCampo();
 }
 
-function limparCampo(){
+function limparCampo() {
     chute = document.querySelector('input');
     chute.value = '';
 }
 
-function exibeAmigos(){
+function exibeAmigos() {
     let lista = document.getElementById('listaAmigos');
     lista.innerHTML = "";
-    for (i = 0; i < (listaDeAmigos.length); i++){
+    for (i = 0; i < (listaDeAmigos.length); i++) {
         let listaLi = document.createElement('li'); // cria um elemento tipo 'li'
         listaLi.textContent = listaDeAmigos[i]; // define o texto do li como o nome naquela posição
         lista.appendChild(listaLi); // adiciona <li> ao <ul> 
     }
 }
 
-function sortearAmigo(){
-    let lista = document.getElementById('listaAmigos');
-    lista.innerHTML = "";
-    let numeroAleatorio = Math.floor((Math.random() * listaDeAmigos.length));
-    let nomeEscolhido = listaDeAmigos[numeroAleatorio];
-    let amigoSorteado = document.getElementById('resultado');
-    
-    amigoSorteado.innerHTML = `<li>O amigo secreto sorteado é: ${nomeEscolhido} </li>`;
+function sortearAmigo() {
+    //verificando se a lista contém algum elemento
+    if (listaDeAmigos.length > 0) {
+        //limpando a lista de nomes antes de mostrar o resultado
+        let lista = document.getElementById('listaAmigos');
+        lista.innerHTML = "";
+
+        let numeroAleatorio = Math.floor((Math.random() * listaDeAmigos.length));
+        let nomeEscolhido = listaDeAmigos[numeroAleatorio];
+
+        if (listaDeSorteados.includes(nomeEscolhido)) {
+            if (listaDeSorteados.length == listaDeAmigos.length) {
+                alert("Você já sorteou todo mundo!" + "\n" + "Adicione mais amigos ou atualize a página para começar de novo");
+            } else {
+                return sortearAmigo(); // recursão para sortear outro amigo se ele já tiver sido sorteado anteriormente
+            }
+        } else {
+            listaDeSorteados.push(nomeEscolhido);
+            let amigoSorteado = document.getElementById('resultado');
+            amigoSorteado.innerHTML = `<li>O amigo secreto sorteado é: ${nomeEscolhido} </li>`;
+        }
+    } else{
+        alert('Você ainda não adicionou nenhum amigo!\n Adicione algum nome para começar');
+    }
+
+
+
+
 }
